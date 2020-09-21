25e320abe114a763d10f449796f7d6aabf68aec2
simonramosb <simonramosb@gmail.com>
2020-09-21 19:25:17 -0300

    navmesh

---
+++ Assets/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class player : MonoBehaviour
+{
+
+    public Transform objetivo;
+    private NavMeshAgent agente;
+
+    void Start()
+    {
+         agente = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agente.destination = objetivo.position;
+    }
+}
